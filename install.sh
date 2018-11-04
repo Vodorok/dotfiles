07a7e63b0667389f64a64021d4179f2693c30dbc
Vodorok <vodorok@gmail.com>
2018-11-04 00:30:31 +0000

    proper install of vim plugins, altough with sudo

--- install.sh
+++ install.sh
@@ -13,3 +13,16 @@ git config --global user.name "Vodorok"
 ln -fsv ~/dotfiles/vimrc ~/.vimrc
 ln -fsv ~/dotfiles/bash_aliases ~/.bash_aliases
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/bundle/nerdtree-git-plugin
+git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+cp -Rv ~/.vim/bundle/vim-colors-solarized/colors ~/.vim
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+
+sudo apt install build-essential cmake python3-dev
+git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+cd ~/.vim/bundle/YouCompleteMe 
+git submodule update --init --recursive
+cd ~
+python3  ~/.vim/bundle/YouCompleteMe/install.py --java-completer
